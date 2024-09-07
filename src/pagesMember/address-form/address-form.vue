3b75a51b161ee9830065a570f503a0b0450e81b5
766674500 <766674500@qq.com>
2024-09-07 10:50:26 +0800

    城市选择

--- src/pagesMember/address-form/address-form.vue
+++ src/pagesMember/address-form/address-form.vue
@@ -28,6 +28,7 @@ const query = defineProps<{
 uni.setNavigationBarTitle({ title: query.id ? '修改地址' : '新建地址' })
 
 // 收集所在地区
+// #ifdef MP-WEIXIN
 const onRegionChange: UniHelper.RegionPickerOnChange = (ev) => {
   // 省市区(前端展示)
   form.value.fullLocation = ev.detail.value.join(' ')
@@ -36,6 +37,16 @@ const onRegionChange: UniHelper.RegionPickerOnChange = (ev) => {
   // 合并数据
   Object.assign(form.value, { provinceCode, cityCode, countyCode })
 }
+// #endif
+
+// #ifdef APP-PLUS || H5
+const onCityChange: UniHelper.UniDataPickerOnChange = (ev) => {
+  // 省市区(后端参数)
+  const [provinceCode, cityCode, countyCode] = ev.detail.value.map((v) => v.value)
+  // 合并数据
+  Object.assign(form.value, { provinceCode, cityCode, countyCode })
+}
+// #endif
 
 // 收集是否默认收货地址
 const onSwitchChange: UniHelper.SwitchOnChange = (ev) => {
@@ -53,7 +64,7 @@ const rules: UniHelper.UniFormsRules = {
       { pattern: /^1[3-9]\d{9}$/, errorMessage: '手机号格式不正确' },
     ],
   },
-  fullLocation: {
+  countyCode: {
     rules: [{ required: true, errorMessage: '请选择所在地区' }],
   },
   address: {
@@ -121,8 +132,9 @@ onLoad(() => {
           v-model="form.contact"
         />
       </uni-forms-item>
-      <uni-forms-item name="fullLocation" class="form-item">
+      <uni-forms-item name="countyCode" class="form-item">
         <text class="label">所在地区</text>
+        <!-- #ifdef MP-WEIXIN -->
         <picker
           class="picker"
           @change="onRegionChange"
@@ -132,6 +144,23 @@ onLoad(() => {
           <view v-if="form.fullLocation">{{ form.fullLocation }}</view>
           <view v-else class="placeholder">请选择省/市/区(县)</view>
         </picker>
+        <!-- #endif -->
+        <!-- #ifdef H5 || APP-PLUS -->
+        <uni-data-picker
+          placeholder="请选择地址"
+          popup-title="请选择城市"
+          collection="opendb-city-china"
+          field="code as value, name as text"
+          orderby="value asc"
+          :step-searh="true"
+          self-field="code"
+          parent-field="parent_code"
+          :clear-icon="false"
+          @change="onCityChange"
+          v-model="form.countyCode"
+        >
+        </uni-data-picker>
+        <!-- #endif -->
       </uni-forms-item>
       <uni-forms-item name="address" class="form-item">
         <text class="label">详细地址</text>
@@ -153,6 +182,12 @@ onLoad(() => {
 </template>
 
 <style lang="scss">
+/* #ifdef H5 || APP-PLUS */
+:deep(.selected-area) {
+  height: auto;
+  flex: 0 1 auto;
+}
+/* #endif */
 page {
   background-color: #f4f4f4;
 }
